8d6a4d4954523e64688a4a0c06ecc435ffb2da1e
eduardo0804 <jesuslazbra1008@outlook.com>
2025-01-20 14:17:43 -0500

    Subiendo Cambios en Variables

--- components/Variables.jsx
+++ components/Variables.jsx
@@ -7,16 +7,25 @@ const numeros = [
     text: "Total Ventas",
   },
   {
-    count: 821250,
-    text: "Capacidad Anual",
+    count: 2250,
+    text: "Capacidad Diaria",
   },
   {
     count: 400000,
     text: "Total Clientes",
   },
   {
-    count: 2250,
-    text: "Capacidad Diaria",
+    count: -800000,
+    text: "Margen Operativo",
+  },
+  {
+    count: 821250,
+    text: "Capacidad Anual",
+  },
+  {
+    count: 48.71,
+    text: "Porcentaje Ocupación",
+    isPercentage: true, // Identifica que este debe incluir decimales
   },
 ];
 
@@ -24,32 +33,33 @@ const Variables = () => {
   return (
     <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
-          {numeros.map((item, index) => {
-            return (
-              <div
-                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
-                key={index}
+        <div className="grid grid-cols-1 gap-y-8 xl:grid-cols-3 xl:gap-8 max-w-[80vw] mx-auto">
+          {numeros.map((item, index) => (
+            <div
+              className="flex gap-4 items-center justify-center xl:justify-start"
+              key={index}
+            >
+              <CountUp
+                end={item.count}
+                duration={10}
+                delay={3}
+                decimals={item.isPercentage ? 2 : 0} // Decimales para porcentajes
+                className="text-4xl xl:text-6xl font-extrabold"
+              />
+              {item.isPercentage && (
+                <span className="text-4xl xl:text-6xl font-extrabold">%</span>
+              )}
+              <p
+                className={`${
+                  item.text.length < 15 ? "max-w-[100px]" : "max-w-[140px]"
+                } leading-snug text-white/80`}
               >
-                <CountUp
-                  end={item.count}
-                  duration={10}
-                  delay={3}
-                  className="text-4xl xl:text-6xl font-extrabold"
-                />
-                <p
-                  className={`${
-                    item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
-                  } leading-snug text-white/80`}
-                >
-                  {item.text}
-                </p>
-              </div>
-            );
-          })}
+                {item.text}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
-      ;
     </section>
   );
 };
